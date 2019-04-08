66f80a50a2db0267fd142c0da2962105fe32ad3e
Xiaoxu <xiaoxu.gu.cn@gmail.com>
2019-04-08 14:27:11 -0400

    card list title and bill total

--- src/app/bill/total-bill/total-bill.component.ts
+++ src/app/bill/total-bill/total-bill.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BillDataService } from '../bill-data.service';
 
 @Component({
   selector: 'app-total-bill',
   templateUrl: './total-bill.component.html',
   styleUrls: ['./total-bill.component.css']
 })
-export class TotalBillComponent implements OnInit {
+export class TotalBillComponent implements OnInit, OnDestroy {
+  billListSubscription: any;
+  totalIncome: number;
+  totalOutcome: number;
+  balance: number;
 
-  constructor() { }
+  constructor(private billDataService: BillDataService) { }
 
   ngOnInit() {
+    this.billListSubscription = this.billDataService.billListEventEmitter.subscribe(
+      (bills) => {
+        let ti = 0;
+        let to = 0;
+        bills.forEach((ele) => {
+          if (ele.type === 'Income') {
+            ti += ele.amount;
+          } else if (ele.type === 'Outcome') {
+            to += ele.amount;
+          }
+        });
+        this.totalIncome = ti;
+        this.totalOutcome = to;
+        this.balance = ti - to;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.billListSubscription.unsubscribe();
   }
 
 }
